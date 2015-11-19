27e77c878fee23f6e0ad09f4b5235db091c84614
Quang Do <qdo@evernote.com>
2015-11-19 10:48:54 -0800

    daily commit

--- ex1-printing-press.py
+++
@@ -1,8 +0,0 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typicing this."
-print "This is fun."
-print 'Yaya! Printing.'
-print "Id much rather you 'not'."
-print 'I "said" do not touch this.'
-print 'Extra credit baby!'
